138107f4deb7ed69172dc24931e41cdb7331e610
Kairo Ribeiro <kairo10@gmail.com>
2022-06-20 13:34:06 -0400

    viewing all flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,8 +1,44 @@
+import { Flight } from '../models/flight.js'
+
+
 function newFlight(req, res) {
-    res.render('flights/new')
+    res.render('flights/new',{
+        title: 'Add Flight'
+    })
         
 }
 
+
+
+function create(req, res) {
+
+    for (let key in req.body){
+        if (req.body[key] === '') delete req.body[key]
+    }
+    Flight.create(req.body)
+    .then(flight => {
+      res.redirect(`/flights`)
+    })
+    .catch(err => {
+      res.redirect('/flights')
+    })
+  }
+
+
+
+function index(req,res) {
+    Flight.find({})
+    .then(flights => {
+        res.render('flights/index',{
+            flights: flights,
+            title: 'All Flights',
+        })
+    })
+}
+
+
 export {
-    newFlight as new
+    newFlight as new,
+    create,
+    index,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,8 +3,16 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+
+//GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
+
 /* GET users listing. */
 router.get('/new', flightsCtrl.new)
+
+//POST localhost:3000/flights
+router.post('/', flightsCtrl.create)
 
 export {
   router
